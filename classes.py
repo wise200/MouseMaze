097c76923d55d621d5f0aac9b82fed4b6ecd31da
Wise <3015453@stumail.dallasisd.org>
2018-11-23 17:59:58 -0600

    Completed DFS Section
    Completed Mouse, Cell, and BabyCell classes, and performed random Depth First Search in main.py

--- classes.py
+++ classes.py
@@ -1,47 +1,144 @@
 from random import choice
-
+import pygame
+from queue import Queue
+from pygame import Rect
 class Maze:
-	def __init__(self, rows, cols, frameRate=10):
-		self.grid = [[BabyCell(row,col,self) for col in range(cols)] for row in range(rows)]
-		for row in self.grid:
+	def __init__(self, rows, cols):
+		self.maze = [[BabyCell(row,col,self) for col in range(cols)] for row in range(rows)]
+		for row in self.maze:
 			for cell in row:
 				cell.addNeighbors()
 		#Create Maze with Recursive Backtracking
-		stack = [self.grid[0][0]]
-		self.grid[0][1].neighbors.remove(self.grid[0][0])
-		self.grid[1][0].neighbors.remove(self.grid[0][0])
+		stack = [self.maze[0][0]]
+		#self.maze[0][1].neighbors.remove(self.maze[0][0])
+		#self.maze[1][0].neighbors.remove(self.maze[0][0])
+		self.maze[0][0].visited = True
 		while len(stack) > 0:
 			cell = stack.pop()
 			if cell.hasNeighbors():
 				stack.append(cell)
 				stack.append(cell.popNeighbor())
-		 
+		#self.maze[0][0].wall, self.maze[0][0].floor = False, False
+		#Create traversable graph from maze
+		self.graph = [[Cell(row,col) for col in range(cols)] for row in range(rows)]
+		for r in range(rows):
+			for c in range(cols):
+				cell = self.graph[r][c]
+				babyCell = self.maze[r][c]
+				if not babyCell.wall:
+					cell.addNeighbor(self.graph[r][c+1])
+				if not babyCell.floor:
+					cell.addNeighbor(self.graph[r+1][c])
+		for row in self.maze:
+			for cell in row:
+				cell.visited = False
+		self.startScreen()
+		self.mouse = Mouse(self.graph[0][0], self.boxSize)
+		
+	def startScreen(self):
+		pygame.init()
+		displayInfo = pygame.display.Info()
+		dims = (displayInfo.current_w, displayInfo.current_h)
+		self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+		width = dims[0] // len(self.maze[0])
+		height = dims[1] // len(self.maze)
+		self.boxSize = min(width, height) - 17
+		self.sizeCalc = self.boxSize+1
+		self.left = (dims[0] - self.sizeCalc * len(self.maze[0])) // 2
+		self.top = 125
+		self.clock = pygame.time.Clock()
+		
+	def show(self, frameRate=5):
+		while not self.mouse.queue.empty():
+			self.clock.tick(frameRate)
+			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+					sys.exit(pygame.quit())
+			self.draw()
+			self.getCell(self.mouse.node).visited = True
+			self.mouse.node = self.mouse.queue.get()
+			
+	def draw(self):
+		self.screen.fill((0,0,0))
+		white = (255,255,255)
+		purple = (150,0,150)
+		#Draw maze
+		outline = pygame.Rect(self.left, self.top, len(self.maze[0]) * self.sizeCalc, len(self.maze) * self.sizeCalc)
+		pygame.draw.rect(self.screen, white, outline, 1)
+		for row in self.maze:
+			for cell in row:
+				x = (cell.col+1) * self.sizeCalc + self.left
+				y = (cell.row+1) * self.sizeCalc + self.top
+				if cell.floor:
+					pygame.draw.line(self.screen, white, (x-self.boxSize,y), (x,y))
+				if cell.wall:
+					pygame.draw.line(self.screen, white, (x,y-self.boxSize), (x,y))
+				if cell.visited:
+					rect = Rect(x-self.boxSize, y-self.boxSize, self.boxSize, self.boxSize)
+					pygame.draw.rect(self.screen, purple, rect)
+		#draw mouse
+		x = self.mouse.node.col * self.sizeCalc + self.left
+		y = self.mouse.node.row * self.sizeCalc + self.top
+		
+		rect = Rect(x, y, self.boxSize, self.boxSize)
+		self.screen.blit(self.mouse.img, rect)
+		pygame.display.flip()
+		
+	def getCell(self, node):
+		return self.maze[node.row][node.col]
 
+class Mouse:
+	def __init__(self, node, size):
+		self.node = node
+		self.img = pygame.image.load('mouse.png').convert_alpha()
+		self.img = pygame.transform.scale(self.img, (size, size))
+		self.size = size
+		self.sizeCalc = size + 1
+		self.queue = Queue()
+		
+	def moveTo(self, cell):
+		self.queue.put(cell)
+
+class Cell:
+	def __init__(self, row, col):
+		self.row = row
+		self.col = col
+		self.neighbors = []
+		
+	def addNeighbor(self, neighbor):
+		self.neighbors.append(neighbor)
+		neighbor.neighbors.append(self)
+		
 class BabyCell:
 	def __init__(self, row, col, grid):
 		self.row = row
 		self.col = col
-		self.grid = grid
+		self.maze = grid
 		self.floor = True
 		self.wall = True
-		self.neighbors = {}
+		self.neighbors =set()
+		self.visited = False
 	
 	def addNeighbors(self):
-		self.neighbors += self.top()
-		self.neighbors += self.bottom()
-		self.neighbors += self.left()
-		self.neighbors += self.right()
+		self.neighbors.add(self.top())
+		self.neighbors.add(self.bottom())
+		self.neighbors.add(self.left())
+		self.neighbors.add(self.right())
 		self.neighbors.discard(None)
 	
 	def hasNeighbors(self):
-		return len(self.neighbors) > 0
+		for nb in self.neighbors:
+			if not nb.visited:
+				return True
+		return False
 	
 	def popNeighbor(self):
-		neighbor = self.neighbors.remove(choice(tuple(self.neighbors)))
+		nbs = [nb for nb in self.neighbors if not nb.visited]
+		neighbor = choice(nbs)
+		neighbor.visited = True
 		self.breakWall(neighbor)
-		for cell in neighbor.neighbors:
-			cell.neighbors.remove(neighbor)
 		return neighbor
+		
 	
 	def breakWall(self, neighbor):
 		if neighbor.row > self.row:
@@ -54,13 +151,13 @@ class BabyCell:
 			neighbor.wall = False
 	
 	def top(self):
-		return self.grid[self.row-1][col] if self.row > 0 else None
+		return self.maze.maze[self.row-1][self.col] if self.row > 0 else None
 	
 	def bottom(self):
-		return self.grid[self.row+1][col] if sel.row < len(self.grid)-1 else None
+		return self.maze.maze[self.row+1][self.col] if self.row < len(self.maze.maze)-1 else None
 	
 	def left(self):
-		return self.grid[self.row][col-1] if sel.col > 0 else None
+		return self.maze.maze[self.row][self.col-1] if self.col > 0 else None
 	
 	def right(self):
-		return self.grid[self.row][col+1] if sel.col < len(self.grid[self.row])-1 else None
+		return self.maze.maze[self.row][self.col+1] if self.col < len(self.maze.maze[self.row])-1 else None
---
+++ main.py
@@ -0,0 +1,17 @@
+from classes import Maze
+from random import choice
+maze = Maze(15,15)
+mouse = maze.mouse
+visited = set()
+stack = [mouse.node]
+visited.add(mouse.node)
+while len(stack) > 0:
+	cell = stack.pop()
+	mouse.moveTo(cell)
+	nbs = [nb for nb in cell.neighbors if nb not in visited]
+	if len(nbs) > 0:
+		nb = choice(nbs)
+		visited.add(nb)
+		stack.append(cell)
+		stack.append(nb)
+maze.show(15)
