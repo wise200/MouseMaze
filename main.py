9df6733f60b0b2c4d05cd8e53ff42cbc0a170b92
Wise <3015453@stumail.dallasisd.org>
2018-11-24 23:17:23 -0600

    Added cheese removal to main.py

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ mouse = maze.mouse
 visited = set()
 stack = [mouse.node]
 visited.add(mouse.node)
+count = 0
 while len(stack) > 0:
 	cell = stack.pop()
 	mouse.moveTo(cell)
+	if cell.hasCheese:
+		count += 1
+		cell.hasCheese = False
 	nbs = [nb for nb in cell.neighbors if nb not in visited]
 	if len(nbs) > 0:
 		nb = choice(nbs)
 		visited.add(nb)
 		stack.append(cell)
 		stack.append(nb)
-maze.show(0,True)
+maze.show(0)
